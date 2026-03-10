ca85b78ae727e0526190645ea9fe519f2a6a61aa
h.yan.leedstrinity <h.yan@leedstrinity.ac.uk>
2026-03-10 19:42:29 +0000

    initial commit

---
+++ frontend/js/script.js
@@ -0,0 +1,10 @@
+// Simple JavaScript function
+function showMessage(){
+    alert("Hello! This message is from JavaScript.");
+}
+
+
+// The second function
+function showPageInfo(){
+    alert("You are currently viewing the second page.");
+}
